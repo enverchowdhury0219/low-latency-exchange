a30c8038b9e56103ec45869568fa881b3492e6bd
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2026-08-28 17:37:40 -0500

    created the outline of our orderbook

---
+++ include/exchange/order_book.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstddef>
+#include <deque>
+#include <functional>
+#include <map>
+#include <optional>
+
+#include "exchange/order.hpp"
+
+namespace exchange
+{
+
+// building our orderbook logic, what actually stores these data points
+class OrderBook
+{
+public:
+    void add_order(const Order& order);
+    [[nodiscard]] std::optional<Price> best_bid() const;
+    [[nodiscard]] std::optional<Price> best_ask() const;
+
+    [[nodiscard]] std::size_t bid_level_count() const;
+    [[nodiscard]] std::size_t ask_level_count() const;
+private:
+    using OrdersAtPrice = std::deque<Order>;
+
+    using BidLevels =
+        std::map<Price, OrdersAtPrice, std::greater<Price>>; // we want the highest bid
+
+    using AskLevels =
+        std::map<Price, OrdersAtPrice, std::less<Price>>; // we want the lowest ask
+
+    BidLevels bids_;
+    AskLevels asks_;
+};
+
+}
