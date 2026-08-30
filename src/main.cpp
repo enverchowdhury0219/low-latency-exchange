3c14355a9b2b151cdc997df8413feafbc82c17bb
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2026-08-30 17:44:35 -0500

    tested our trade sweep logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,55 +5,67 @@
 // added 3 buy orders but 2 bid price levels as 2 orders have same price
 int main()
 {
-    exchange::OrderBook sell_test_book;
+   exchange::OrderBook sweep_book;
 
-    sell_test_book.add_order({
-        100,
-        10125,
-        150,
-        exchange::Side::Buy
+    sweep_book.add_order({
+        1,
+        10130,
+        50,
+        exchange::Side::Sell
     });
 
-    sell_test_book.add_order({
-        101,
-        10120,
-        200,
-        exchange::Side::Buy
+    sweep_book.add_order({
+        2,
+        10130,
+        75,
+        exchange::Side::Sell
     });
 
-    exchange::Order aggressive_sell{
-        200,
-        10110,
+    sweep_book.add_order({
+        3,
+        10135,
         100,
         exchange::Side::Sell
+    });
+
+    sweep_book.add_order({
+        4,
+        10145,
+        200,
+        exchange::Side::Sell
+    });
+
+    exchange::Order sweep_buy{
+    20,
+    10140,
+    200,
+    exchange::Side::Buy
     };
 
-    const auto sell_trade =
-        sell_test_book.execute_one(aggressive_sell);
+    const auto trades = sweep_book.execute(sweep_buy);
 
-    if (sell_trade)
+    std::cout << "\nSweep produced "
+          << trades.size()
+          << " trades\n";
+
+    for (const auto& trade : trades)
     {
-        std::cout << "\nSell-side trade executed\n";
-        std::cout << "Buyer: "
-                << sell_trade->buy_order_id << '\n';
-
-        std::cout << "Seller: "
-                << sell_trade->sell_order_id << '\n';
-
-        std::cout << "Price: "
-                << sell_trade->price << '\n';
-
-        std::cout << "Quantity: "
-                << sell_trade->quantity << '\n';
+        std::cout
+            << "Buyer #" << trade.buy_order_id
+            << " matched Seller #" << trade.sell_order_id
+            << " | Price: " << trade.price
+            << " | Quantity: " << trade.quantity
+            << '\n';
     }
 
-    std::cout << "Incoming sell remaining: "
-            << aggressive_sell.quantity
+    std::cout << "Incoming remaining quantity: "
+            << sweep_buy.quantity
             << '\n';
 
-    std::cout << "Best bid after trade: "
-            << *sell_test_book.best_bid()
+    std::cout << "Best ask after sweep: "
+            << *sweep_book.best_ask()
             << '\n';
-    return 0;
+
+
 }
 
