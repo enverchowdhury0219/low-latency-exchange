ffe860087dc3cbb526518e77bef0f41666b9771c
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2026-09-01 00:28:58 -0400

    testing submit function logic

--- src/main.cpp
+++ src/main.cpp
@@ -5,67 +5,64 @@
 // added 3 buy orders but 2 bid price levels as 2 orders have same price
 int main()
 {
-   exchange::OrderBook sweep_book;
+    exchange::OrderBook remainder_book;
 
-    sweep_book.add_order({
+    remainder_book.add_order({
         1,
         10130,
         50,
         exchange::Side::Sell
     });
 
-    sweep_book.add_order({
+    remainder_book.add_order({
         2,
-        10130,
+        10135,
         75,
         exchange::Side::Sell
     });
 
-    sweep_book.add_order({
+    remainder_book.add_order({
         3,
-        10135,
+        10145,
         100,
         exchange::Side::Sell
     });
 
-    sweep_book.add_order({
-        4,
-        10145,
-        200,
-        exchange::Side::Sell
-    });
-
-    exchange::Order sweep_buy{
+    exchange::Order incoming_buy{
     20,
     10140,
     200,
     exchange::Side::Buy
     };
 
-    const auto trades = sweep_book.execute(sweep_buy);
+    const auto remainder_trades =
+    remainder_book.submit(incoming_buy);
 
-    std::cout << "\nSweep produced "
-          << trades.size()
-          << " trades\n";
+    std::cout << "\nRemainder test\n";
 
-    for (const auto& trade : trades)
+    for (const auto& trade : remainder_trades)
     {
         std::cout
-            << "Buyer #" << trade.buy_order_id
-            << " matched Seller #" << trade.sell_order_id
-            << " | Price: " << trade.price
-            << " | Quantity: " << trade.quantity
+            << "Trade: "
+            << trade.quantity
+            << " @ "
+            << trade.price
             << '\n';
     }
 
-    std::cout << "Incoming remaining quantity: "
-            << sweep_buy.quantity
-            << '\n';
+    if (const auto bid = remainder_book.best_bid())
+    {
+        std::cout << "Best bid after submission: "
+                << *bid
+                << '\n';
+    }
 
-    std::cout << "Best ask after sweep: "
-            << *sweep_book.best_ask()
-            << '\n';
-
+    if (const auto ask = remainder_book.best_ask())
+    {
+        std::cout << "Best ask after submission: "
+                << *ask
+                << '\n';
+    }
 
 }
 
