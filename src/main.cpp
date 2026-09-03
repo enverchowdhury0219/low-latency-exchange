b6fd7ab8cd38897cf344bdcf6d4870f2e5d4fb55
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2026-09-03 14:13:33 -0500

    testing our duplicate fill id logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,41 @@
 // added 3 buy orders but 2 bid price levels as 2 orders have same price
 int main()
 {
-    exchange::OrderBook duplicate_test;
+    exchange::OrderBook reuse_test;
 
-    (void)duplicate_test.submit({
-        42,
-        10120,
+    (void)reuse_test.submit({
+        50,
+        10130,
+        100,
+        exchange::Side::Sell
+    });
+
+    const auto fill_trades = reuse_test.submit({
+        51,
+        10130,
         100,
         exchange::Side::Buy
     });
 
     try
     {
-        (void)duplicate_test.submit({
-            42,
-            10110,
+        (void)reuse_test.submit({
             50,
-            exchange::Side::Buy
+            10140,
+            100,
+            exchange::Side::Sell
         });
+
+        std::cout << "Order ID 50 successfully reused\n";
+        std::cout << "Order ID traded: " << fill_trades.OrderId << ".";
     }
     catch (const std::invalid_argument& error)
     {
-        std::cout << "Rejected duplicate: "
-                << error.what()
-                << '\n';
+        std::cout << error.what() << '\n';
     }
-
     
+
+
 
 
 }
