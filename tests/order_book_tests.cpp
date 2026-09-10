47935543617ed0d7daaf2850c1ee6ede876244f5
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2026-09-10 10:26:04 -0400

    created blank test space for best bid and ask

--- tests/order_book_tests.cpp
+++ tests/order_book_tests.cpp
@@ -3,3 +3,19 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+
+#include "exchange/order_book.hpp"
+
+void expect(bool condition, const std::string& message)
+{
+    if (!condition)
+    {
+        throw std::runtime_error(message);
+    }
+}
+
+void test_best_bid_and_ask()
+{
+
+    
+}
