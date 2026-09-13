9362148c5b8697f21130594dadbaa33e0f6380e3
enver chowdhury <enver.chowdhury@mail.utoronto.ca>
2026-09-13 10:51:42 -0400

    implemented best bid and ask test

--- tests/order_book_tests.cpp
+++ tests/order_book_tests.cpp
@@ -15,8 +15,47 @@ void expect(bool condition, const std::string& message)
     }
 }
 
+
+// testing if our best bid and ask function works 
 void test_best_bid_and_ask()
 {
+    exchange::OrderBook book;
 
+    (void)book.submit({
+        1,
+        10125,
+        100,
+        exchange::Side::Buy
+    });
+
+    (void)book.submit({
+        2,
+        10125,
+        200,
+        exchange::Side::Buy
+    });
+
+     (void)book.submit({
+        3,
+        10130,
+        150,
+        exchange::Side::Sell
+    });
+
+    (void)book.submit({
+        4,
+        10135,
+        75,
+        exchange::Side::Sell
+    });
+
+    const auto bid = book.best_bid();
+    const auto ask = book.best_ask();
+
+    expect(bid.has_value(), "Best bid should exist");
+    expect(ask.has_value(), "Best ask should exist");
+
+    expect(*bid == 10125, "Best bid should be 10125");
+    expect(*ask == 10130, "Best ask should be 10130");
     
 }
